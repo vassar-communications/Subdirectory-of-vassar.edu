87efc17deb054174801e23cafe9cc65eff43ddcb
Jeff Macaluso <samacaluso@vassar.edu>
2017-10-26 14:53:31 -0400

    initial commit
    About subdirectory at root of vassar.edu. Asset and config.php files at
    the root and in the About subdirectory.

---
+++ assets/config.php
@@ -0,0 +1,12 @@
+<?php
+return 
+array(
+    'siteDesignType' => 'academic',
+    'vassarCollegeAppend' => '',
+    // 'subSiteNavigationAdditional' => array('grandparent','All Exhibits'),
+    'typekitID' => 'gon4vhs',
+    'siteNameHeader' => "
+<b class='u-Masthead__siteName--line1'>About</b>",
+    'siteNameSubhead' => "
+<b class='u-Masthead__siteName--line2'>Second Level Subdirectory</b>",
+);
---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return 
+array(
+    'siteDesignType' => 'academic',
+    'vassarCollegeAppend' => '',
+    // 'subSiteNavigationAdditional' => array('grandparent','All Exhibits'),
+    'typekitID' => 'aty2ace',
+    'siteNameHeader' => "About Vassar",
+    // 'siteNameSubhead' => "<b class='u-Masthead__siteName--line2'>Second Level Subdirectory</b>",
+);
